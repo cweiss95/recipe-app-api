226962a664c18dfa7e1bee3c1bd66e7bba3c8560
Charles Weiss <cweiss1995@gmail.com>
2026-02-11 13:38:32 +0800

    Fix linting issues

--- app/core/models.py
+++ app/core/models.py
@@ -3,7 +3,7 @@ Database models.
 """
 
 from django.db import models
-from django.contrib.auth.models import(
+from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin
@@ -31,6 +31,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
 
         return user
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     """User in the system."""
