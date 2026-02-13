a8f649a07dd2d73087c254d4f066798078797b2b
Charles Weiss <cweiss1995@gmail.com>
2026-02-13 09:54:10 +0800

    Add creation of tags and deletion of tags. Fix linting

--- app/recipe/serialisers.py
+++ app/recipe/serialisers.py
@@ -1,14 +1,14 @@
 """Serialisers for recipe API."""
 from rest_framework import serializers
-from core.models import Recipe
+from core.models import Recipe, Tag
 
 
-class RecipeSerialiser(serializers.ModelSerializer):
-    """Serialiser for recipe objects."""
+class TagSerialiser(serializers.ModelSerializer):
+    """Serialiser for tag objects."""
 
     class Meta:
-        model = Recipe
-        fields = ['id', 'user', 'title', 'time_minutes', 'price', 'link']
+        model = Tag
+        fields = ['id', 'user', 'name']
         read_only_fields = ['id', 'user']
 
     def validate(self, attrs):
@@ -22,6 +22,41 @@ class RecipeSerialiser(serializers.ModelSerializer):
                         {field_name: 'This field is read-only.'}
                     )
         return super().validate(attrs)
+
+
+class RecipeSerialiser(serializers.ModelSerializer):
+    """Serialiser for recipe objects."""
+    tags = TagSerialiser(many=True, required=False)
+
+    class Meta:
+        model = Recipe
+        fields = [
+            'id', 'user', 'title', 'time_minutes',
+            'price', 'link', 'tags']
+        read_only_fields = ['id', 'user']
+
+    def validate(self, attrs):
+        """Validate and raise validation error if
+           readonly fields during an update."""
+        if self.instance:
+            for field_name in self.initial_data:
+                field = self.get_fields().get(field_name)
+                if field and field.read_only:
+                    raise serializers.ValidationError(
+                        {field_name: 'This field is read-only.'}
+                    )
+        return super().validate(attrs)
+
+    def create(self, validated_data):
+        """Create a recipe."""
+        tags_data = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        auth_user = self.context['request'].user
+        for tag in tags_data:
+            tag_obj, created = Tag.objects.get_or_create(user=auth_user, **tag)
+            recipe.tags.add(tag_obj)
+
+        return recipe
 
 
 class RecipeDetailSerialiser(RecipeSerialiser):
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -1,11 +1,11 @@
 """
 Views for recipe APIs.
 """
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe
+from core.models import Recipe, Tag
 
 from recipe import serialisers
 
@@ -31,3 +31,22 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         """Create a new recipe."""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(
+    mixins.ListModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.DestroyModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
+
+    """View for manage tag APIs."""
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    serializer_class = serialisers.TagSerialiser
+    queryset = Tag.objects.all()
+
+    def get_queryset(self):
+        """Return objects for the current authenticated user only."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
